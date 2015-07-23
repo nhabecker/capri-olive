d0ab6f8a3a1fb8402adb7f4d7c0512c37ebb971f
Nancy Habecker <nancydujmo@benedict-cumberbatch.local>
2015-07-22 21:00:39 -0400

    second commit

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var wunderlistSecret = process.env.WUNDERLIST_SECRET;
 
 app.use(bodyParser.json()); // support json encoded bodies
 
+app.get('/', function (req, res) {
+   res.send({ message: 'hello world!' });
+});
+
 app.post('/wunderlist/callback', function (req, res) {
     var code = req.body.code,
         state = req.body.state;
